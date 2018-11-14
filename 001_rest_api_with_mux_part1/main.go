97955c275ac1ae5a9c25b3736b8585f7562d43c6
muhammad ridho <ridho@muhammads-Mac-mini.local>
2018-11-14 21:14:24 +0700

    add func getData, getDatabyId

--- 001_rest_api_with_mux_part1/main.go
+++ 001_rest_api_with_mux_part1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,12 +9,46 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// car struct
+type Car struct {
+	ID          string `json:"id"`
+	Manufacture string `json:"manufacture"`
+	Product     string `json:"product"`
+	Year        string `json:"year"`
+	Engine      string `json:"engine"`
+	Model       *Model `json:"model"`
+}
+
+// Model struc
+type Model struct {
+	Code  string `json:"code"`
+	Type  string `json:"type"`
+	Price string `json:"price"`
+	Color string `json:"color"`
+}
+
+// initiate car var as a slice car struc
+var cars []Car
+
 func getCars(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("sameone call me in rest api")
+	fmt.Println("sameone call me in rest api getCars")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(cars)
 }
 
 func getCar(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("sameone call me in rest api getCars")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get param from browser
 
+	for _, item := range cars {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(&Car{})
 }
 
 func createCars(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +69,14 @@ func main() {
 
 	// init router
 	r := mux.NewRouter()
+
+	// hardcode data cars
+	// , Code: "2CRV", Type: "Disel", Price: "550 jt", Color: "Silver", Code: "3CRV", Type: "Practise", Price: "450 jt", Color: "Black"
+	// , Code: "2HRV", Type: "Sport", Price: "310 jt", Color: "Brown"
+	// , Code: "2DX", Type: "TwinLamp", Price: "10 jt", Color: "white"
+	cars = append(cars, Car{ID: "1", Manufacture: "Honda", Product: "CRV", Year: "2018", Engine: "20L", Model: &Model{Code: "1CRV", Type: "Facelift", Price: "500 jt", Color: "white"}})
+	cars = append(cars, Car{ID: "2", Manufacture: "Honda", Product: "HRV", Year: "2018", Engine: "15L", Model: &Model{Code: "1HRV", Type: "Facelift", Price: "300 jt", Color: "Red"}})
+	cars = append(cars, Car{ID: "3", Manufacture: "Toyota", Product: "DX", Year: "1990", Engine: "15L", Model: &Model{Code: "1DX", Type: "QuardLamp", Price: "20 jt", Color: "Red"}})
 
 	// route and handle endpoint
 	r.HandleFunc("/cars", getCars).Methods("GET")
