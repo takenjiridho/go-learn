4cc2e6584c8c35884caf7f77cf5b4a705389d8ef
muhammad ridho <ridho@desktop-36.indonesiapower.corp>
2018-12-21 08:33:26 +0700

    success modular connection to driver module

--- 11_glide_oracle_mux/driver/driver.go
+++ 11_glide_oracle_mux/driver/driver.go
@@ -6,17 +6,34 @@ import (
 	"os"
 
 	_ "gopkg.in/goracle.v2"
+	// _ "github.com/mattn/go-oci8"
 )
 
-var db *sql.DB
+// const dsn = `ICMS/icmsganteng@192.168.100.75:1521/devicms`
 
-func connectDB() *sql.DB {
+// const dsn = `ICMS/icmsganteng@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=tcp)(HOST=192.168.100.75)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=devicms)))`
+
+// var db *sql.DB
+
+func ConnectDB() (*sql.DB, error) {
+
+	// fmt.Println("connection string " + os.Getenv("ORACLE_URL"))
+	fmt.Println("driver oracle load..... " + os.Getenv("ORACLE_URL"))
 
 	db, err := sql.Open("goracle", os.Getenv("ORACLE_URL"))
-	if err != nil {
-		fmt.Println(err)
-	}
+	// db, err := sql.Open("oci8", dsn)
 
-	defer db.Close()
-	return db
+	// if err != nil {
+	// 	fmt.Println("errrrror ..... ")
+	// 	fmt.Println(err)
+	// }
+	// defer db.Close()
+	//
+	// err = db.Ping()
+	// if err != nil {
+	// 	fmt.Println("ping ..... ")
+	// 	fmt.Println(err)
+	// }
+
+	return db, err
 }
--- 11_glide_oracle_mux/main.go
+++ 11_glide_oracle_mux/main.go
@@ -1,31 +1,49 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"go-learn/11_glide_oracle_mux/driver"
 
+	// _ "github.com/mattn/go-oci8"
 	"github.com/subosito/gotenv"
+	_ "gopkg.in/goracle.v2"
 )
 
-var db *sql.DB
+const dsn = `ICMS/icmsganteng@192.168.100.75:1521/devicms`
 
 func init() {
 	gotenv.Load()
 }
 
+// var db *sql.DB
+
 func main() {
+	// https://jbowens.org/p/using-oracle-from-golang-on-mac-os-x
 	// jdbc:oracle:thin:@192.168.100.75:1521:devicms
+	// 10.8.0.219
+
 	fmt.Println("glide oracle mux")
 
-	db = driver.ConnectDB()
+	db, err := driver.ConnectDB()
+	// ------------------------------------------------------
+	//  goracle
+	// db, err := sql.Open("goracle", dsn)
+	//
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// // defer db.Close()
+	//
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 
 	rows, err := db.Query("select sysdate from dual")
 
 	if err != nil {
 		fmt.Println("Error running query")
-		fmt.Println(err)
-		// return
+		panic(err)
 	}
 
 	defer rows.Close()
@@ -36,5 +54,31 @@ func main() {
 		rows.Scan(&thedate)
 	}
 	fmt.Printf("The date is: %s\n", thedate)
+
+	// -----------------------------------------------------------
+	// user go-oci
+	// db, err := sql.Open("oci8", dsn)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer db.Close()
+	//
+	// rows, err := db.Query("SELECT sysdate FROM dual")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var name string
+	// 	err = rows.Scan(&name)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Println(name)
+	// }
+	// err = rows.Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 }
